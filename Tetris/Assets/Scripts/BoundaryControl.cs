dac1f94c487533499882ab0d0442f7806d6f007f
nikhil7030 <37480239+nikhil7030@users.noreply.github.com>
2020-01-29 22:09:09 +0530

    29-1-2020
    Added Rotating Function
    Added Level Ending And Respawn
    Fixed Issue With Wrong Spawn Positions

--- Tetris/Assets/Scripts/BoundaryControl.cs
+++ Tetris/Assets/Scripts/BoundaryControl.cs
@@ -2,32 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BoundaryControl : MonoBehaviour
 {
     public touch_Test touchtest;
     private bool canMove;
-    private Vector3 rotation = new Vector3(0f,0f,-90);
+    private Quaternion rotation = new Quaternion(0f,0f,0f,90f);
 
     private Vector3 touchPosition;
     private Rigidbody2D rb;
     private Vector3 direction;
     private float moveSpeed = 10f;
+    private bool tilted;
 
     private void Awake()
     {
         canMove = true;
         touchtest = FindObjectOfType<touch_Test>();
         rb = GetComponent<Rigidbody2D>();
+        
+        tilted = false;
     }
 
     private void FixedUpdate()
     {
+        
         if (canMove == true)
         {
+            
+            if (Input.GetKeyDown("t"))
+            {
+                Debug.Log("Rotate 1");
+                //if (rb.gameObject.name.Equals("Z(Clone)"))
+               // {
+                    if (tilted == false)
+                    {
+                        Debug.Log("Rotate 2");
+                        transform.Rotate(0f, 0f, 90f);
+                        tilted = true;
+                    }
+                    else 
+                    {
+                        Debug.Log("Rotate 2");
+                        transform.Rotate(0f, 0f, 0f);
+                        tilted = false;
+                    }
+                    
+               // }
+            }
+
             if (rb.velocity == new Vector2(0,0))
             {
                 rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
             }
+
             if (Input.touchCount > 0)
             {
                 Touch touch = Input.GetTouch(0);   //Store Touch Position Data
@@ -36,10 +64,34 @@ public class BoundaryControl : MonoBehaviour
                     touchPosition = Camera.main.ScreenToWorldPoint(touch.position);     //Fromula To Calculate Screen Boundary
                     touchPosition.z = 0;
                     direction = (touchPosition - transform.position);   //Move Object Towards Touch Position
-                    rb.velocity = new Vector2(0, direction.y) * moveSpeed;
+                    rb.velocity = new Vector2(direction.x , direction.y) * moveSpeed;
                 }
-                if (touch.phase == TouchPhase.Ended)    //
-                    rb.velocity = Vector2.zero;
+
+                if (touch.phase == TouchPhase.Ended)    //To Stop object when touch is ended
+                { 
+                    //rb.velocity = Vector2.zero; 
+                }
+                if (touch.phase == TouchPhase.Stationary)
+                {
+                    Debug.Log("Rotate 1");
+                    if (rb.gameObject.name.Equals("Z(Clone)"))
+                    {
+                        if (tilted == false)
+                        {
+                            Debug.Log("Rotate 2");
+                            transform.Rotate(0f, 0f, 90f);
+                            tilted = true;
+                        }
+                        else
+                        {
+                            Debug.Log("Rotate 2");
+                            transform.Rotate(0f, 0f, 0f);
+                            tilted = false;
+                        }
+
+                    }
+                }
+
             }
         }
     }
---
+++ Tetris/Assets/Scripts/reset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class reset : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(0);   
+    }
+}
